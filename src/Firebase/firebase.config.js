95c2f660571dd3fc1a52766bc8eb723f085ee866
rafsan3503 <rafsanchowdhuryrudro@gmail.com>
2022-11-07 20:42:37 +0600

    firebase added

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBPFUfctK7oi_usBMNubdTNjQvrlFuRk1c",
+  authDomain: "footeo-d76c1.firebaseapp.com",
+  projectId: "footeo-d76c1",
+  storageBucket: "footeo-d76c1.appspot.com",
+  messagingSenderId: "18175284186",
+  appId: "1:18175284186:web:504ca9a5ef78a09b20e6e3",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
---
+++ src/Pages/ErrorPage/ErrorPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ErrorPage = () => {
+  return <div>this is errorPage</div>;
+};
+
+export default ErrorPage;
